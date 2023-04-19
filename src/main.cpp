762bbf51f1e6f7f5575b69278a909b178e917cc1
Antonio Gonzalez <antonygonz266@gmail.com>
2023-04-19 14:25:53 -0600

    Declaracion de nuevas funciones

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include <EEPROM.h>
 
 //Definicion de funciones
-void Mensaje();
-
+void Mensaje();//lee comando
+void shell();//interpreta el comando para iniciar otras funciones
+void start();//enciende la maquina
+void stop();//detiene la maquina
+void modgan();//cambia las ganancias 
 
 //Variables de Funcion Mensasje
 char datoLeido=1;//dato leido al momento
@@ -26,14 +29,17 @@ void setup()
 
 void loop() 
 {
-  Mensaje();
-
+  if (Serial.available())//revisa que esten entrando datos para obtener el mensaje
+  {
+    Mensaje();
+  }
+  
+  shell();
 }
 
 void Mensaje()
 {
 datoLeido = (char)Serial.read();
-
   if (datoLeido!=ultimoDato)//verifica que la ultima lectura no sea el mismo dato
   {//aunque realmente verifica q
     if (datoLeido!=-1)//verifica que el dato no sea levantamiento de tecla puede ocasionar error a futuro
@@ -49,16 +55,19 @@ datoLeido = (char)Serial.read();
   if (datoLeido==10)//verifica que el dato leido sea enter para terminar la comunicacion 
   {
     //Serial.print("\n");//agrega un salto de linea para diferenciar de la linea anterior
-    
-    Serial.println(comando);//manda el string completo que se almaceno
+    //Sigiente apartado para pruebas
+    Serial.println("Comando actual es:"+comando);
+    Serial.println("Comando anterior es:"+comandoAnterior);
+    //Serial.println(comando);//manda el string completo que se almaceno
     //cont=0; //contador para obtener strings de misma longitud
     comandoAnterior=comando;//copia el comando actual al anterior
     comando="";//resetea la variable del string comando
   }
   ultimoDato=datoLeido;//iguala los datos para la comparacion del siguiente ciclo
   //creo que quitar la ultima linea generara mas errores asi que la voy a dejar
-
-  //delay(500);
-
   return;
+}
+void shell()
+{
+  //Serial.println("no hace nada");
 }
